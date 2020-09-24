4e265e723a2f7cbace94d800b7782f4f64d95ab3
Carlos <carlos.bergamasco@gmail.com>
2020-09-24 15:17:19 -0300

    First commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println("Names: ")
+    for (item : String in args){
+        println("$item")
+    }
+
+    infix fun Int.m(other: Int) = this * other   // 4
+    val myPair = 8 m 5
+    println(myPair)
+
+    var p: Print = Print()
+    println(p.myFun(5, 10))
+    println(p.myFun(8))
+}
