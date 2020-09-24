a5f81dddfdf86c2fd926563efe96f3240f17e205
Carlos <carlos.bergamasco@gmail.com>
2020-09-24 16:49:53 -0300

    Add another use of when clause

--- src/main/kotlin/when.kt
+++ src/main/kotlin/when.kt
@@ -1,9 +1,31 @@
 fun main() {
+//    whenExpr()
+    whenAssigns()
+}
+
+fun whenAssigns() {
+    println(whenAssign("Hello"))
+    println(whenAssign(3.4))
+    println(whenAssign(1))
+    println(whenAssign(MyClass()))
+}
+
+fun whenExpr() {
     cases("Hello")
     cases(1)
     cases(0L)
     cases(MyClass())
     cases("hello")
+}
+
+fun whenAssign(obj: Any) : Any{
+    val result = when (obj){
+        1 -> "One"
+        "Hello" -> 1
+        is Long -> false
+        else -> 42
+    }
+    return result
 }
 
 fun cases(o: Any) {
