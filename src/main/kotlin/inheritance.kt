6eeda0dcca5a53a2adb846038d83d2082d20f403
Carlos <carlos.bergamasco@gmail.com>
2020-09-24 16:16:48 -0300

    Add inheritance samples

---
+++ src/main/kotlin/inheritance.kt
@@ -0,0 +1,46 @@
+// ##### Dogs #########
+
+open class Dog {
+    open fun sayHello(){
+        println("wow wow!")
+    }
+}
+
+class Yorkshire : Dog(){
+    override fun sayHello() {
+        println("wif wif!")
+    }
+}
+// ###### Tigers ########
+open class Tiger (val origin : String){
+    fun sayHello(){
+        println("A tiger from $origin says: grrrrh!")
+    }
+}
+
+class SiberianTiger : Tiger("Siberia")
+
+// ##### Lions #########
+open class Lion(val name: String, val origin:String){
+    fun sayHello(){
+        println("$name tiger from $origin says: graoh!")
+    }
+}
+
+class Asiatic (name:String) : Lion(name = name, "India")
+// ##############
+
+fun main() {
+//    dogs()
+
+//    val tiger : Tiger = SiberianTiger()
+//    tiger.sayHello()
+
+    val lion : Lion = Asiatic("Rufo")
+    lion.sayHello()
+}
+
+private fun dogs() {
+    val dog: Dog = Yorkshire()
+    dog.sayHello()
+}
