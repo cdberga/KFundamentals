ce50b99eab4c72a693d2ef74f4bf19f357650b14
Carlos <carlos.bergamasco@gmail.com>
2020-09-28 16:21:48 -0300

    Add associate and groupBy functions

---
+++ src/main/kotlin/collections/associate_by_group_by.kt
@@ -0,0 +1,19 @@
+package collections
+
+data class Person(val name:String, val city:String, val phone:String)
+
+val people = listOf(
+        Person("John", "Boston", "+1-888-123456"),
+        Person("Sarah", "Munich", "+49-777-789123"),
+        Person("Svyatoslav", "Saint-Petersburg", "+7-999-456789"),
+        Person("Vasilisa", "Saint-Petersburg", "+7-999-123456"))
+
+fun main() {
+    val phoneBook = people.associateBy { it.phone }
+    val cityBook = people.associateBy(Person::phone, Person::city)
+    val peopleCities = people.groupBy(Person::city, Person::name)
+
+    println("Phone book: $phoneBook")
+    println("City book: $cityBook")
+    println("People Cities: $peopleCities")
+}
