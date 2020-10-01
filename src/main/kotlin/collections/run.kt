11aa3ab66f79eda1c2be0ce0b1ab362ed2f92458
Carlos <carlos.bergamasco@gmail.com>
2020-10-01 20:43:32 -0300

    Add run function samples

---
+++ src/main/kotlin/collections/run.kt
@@ -0,0 +1,16 @@
+package collections
+
+fun main() {
+    getNUllableLength(null)
+    getNUllableLength("")
+    getNUllableLength("some string with kotlin")
+}
+
+fun getNUllableLength(ns: String?) {
+    println("for \"$ns\":")
+    ns?.run {
+        println("\tis empty? " + isEmpty())
+        println("\tlength = $length")
+        println(length)
+    }
+}
