072b65058e05f3f2ceb4bc18485b5326987284c1
Carlos <carlos.bergamasco@gmail.com>
2020-10-01 20:36:59 -0300

    Add let function samples

---
+++ src/main/kotlin/collections/let.kt
@@ -0,0 +1,25 @@
+package collections
+
+fun main() {
+    checkEmpty()
+    printNonNull(null)
+    printNonNull("My String")
+}
+
+private fun checkEmpty() {
+    val empty = "test".let {
+        println("CustomPrint: $it".toUpperCase())
+        it.isEmpty()
+    }
+    println(" is empty: $empty")
+}
+
+fun printNonNull(str : String?){
+    println("Printing: \"$str\":")
+
+    str?.let {
+        print("\t")
+        println("customPrint: $it".toUpperCase())
+        println()
+    }
+}
