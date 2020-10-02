d8d3c23df127b75563a49d456a02c1b8a262ce59
Carlos <carlos.bergamasco@gmail.com>
2020-10-02 15:37:27 -0300

    Add props in a map sample

---
+++ src/main/kotlin/delegation/props_in_a_map.kt
@@ -0,0 +1,16 @@
+package delegation
+
+class User(val map: Map<String, Any?>) {
+    val name: String by map
+    val age: Int by map
+}
+
+fun main() {
+    val user = User(mapOf(
+            "name" to "John Doe",
+            "age" to 25,
+            "job" to "Computer Scientist"
+    ))
+    println("name = ${user.name}, age = ${user.age}")
+    println("Job = ${user.map.get("job")}")
+}
